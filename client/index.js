8f8474bb4d6760bf63655cbb262f3934c50a60c6
Simon Bonds <simon.bonds@ombiel.com>
2017-09-28 10:19:54 +0100

    Add in es6 promise to package.

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import { polyfill } from 'es6-promise'; polyfill();
 
 ReactDOM.render(<App />, document.getElementById('root'));
